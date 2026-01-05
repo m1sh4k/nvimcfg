b4d0d01e56a1c2f0798b98e1f029647162191129
m1sh4k <mishaivan373@gmail.com>
2026-01-05 21:22:33 +0300

    fix: treesitter indentation init

--- lua/modules/ts.lua
+++ lua/modules/ts.lua
@@ -1,12 +1,12 @@
 local ts_langs = { 'python', 'c', 'cpp', 'css', 'lua', 'markdown', 'typst' }
 
+local ts = require('nvim-treesitter').install(ts_langs)
 
-require'nvim-treesitter'.install(ts_langs)
 
 vim.api.nvim_create_autocmd('FileType', {
   pattern = ts_langs,
-  callback = function() vim.treesitter.start() end,
+  callback = function()
+    vim.treesitter.start()
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end,
 })
-
-
-vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
